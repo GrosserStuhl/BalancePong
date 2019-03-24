60b185da428a2dc2694b3caea073c460ce5e0ee6
Dimitri <dimitri.rsl@gmail.com>
2019-03-24 11:10:41 +0100

    matrix example tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,34 +2,127 @@
 #include "Arduino.h"
 #include <FastLED.h>
 
-// #define LED_PIN  3
+#define NUM_LEDS 5
 
-// #define COLOR_ORDER GRB
-// #define CHIPSET     WS2811
+// Data pin that led data will be written out over
+#define DATA_PIN 3
 
-// #define BRIGHTNESS 64
+#define COLOR_ORDER GRB
+#define CHIPSET WS2812B
+
+#define BRIGHTNESS 20
+
+// Params for width and height
+const uint8_t kMatrixWidth = 5;
+const uint8_t kMatrixHeight = 1;
+
+// Param for different pixel layouts
+const bool kMatrixSerpentineLayout = true;
+
+CRGB leds[NUM_LEDS];
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
-#define LED_BUILTIN 13
+// #define LED_BUILTIN 13
+
+uint16_t XY(uint8_t x, uint8_t y)
+{
+  uint16_t i;
+
+  if (kMatrixSerpentineLayout == false)
+  {
+    i = (y * kMatrixWidth) + x;
+  }
+
+  if (kMatrixSerpentineLayout == true)
+  {
+    if (y & 0x01)
+    {
+      // Odd rows run backwards
+      uint8_t reverseX = (kMatrixWidth - 1) - x;
+      i = (y * kMatrixWidth) + reverseX;
+    }
+    else
+    {
+      // Even rows run forwards
+      i = (y * kMatrixWidth) + x;
+    }
+  }
+
+  return i;
+}
+
+void DrawOneFrame(byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
+{
+  byte lineStartHue = startHue8;
+  for (byte y = 0; y < kMatrixHeight; y++)
+  {
+    lineStartHue += yHueDelta8;
+    byte pixelHue = lineStartHue;
+    for (byte x = 0; x < kMatrixWidth; x++)
+    {
+      pixelHue += xHueDelta8;
+      leds[XY(x, y)] = CHSV(pixelHue, 255, 255);
+    }
+  }
+}
+
+void doCrazyShit()
+{
+  uint32_t ms = millis();
+  int32_t yHueDelta32 = ((int32_t)cos16(ms * (27 / 1)) * (350 / kMatrixWidth));
+  int32_t xHueDelta32 = ((int32_t)cos16(ms * (39 / 1)) * (310 / kMatrixHeight));
+  DrawOneFrame(ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
+  if (ms < 5000)
+  {
+    FastLED.setBrightness(scale8(BRIGHTNESS, (ms * 256) / 5000));
+  }
+  else
+  {
+    FastLED.setBrightness(BRIGHTNESS);
+  }
+  FastLED.show();
+}
 
 void setup()
 {
+  delay(2000);
+
   // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  //dsafd
+  // pinMode(LED_BUILTIN, OUTPUT);
+
+  FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+  // for (int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1)
+  // {
+  //   // Turn our current led on to white, then show the leds
+  //   leds[whiteLed] = CRGB::White;
 
-  // wait for a second
-  delay(1000);
+  //   // Show the leds (only one of which is set to white, from above)
+  //   FastLED.show();
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+  //   // Wait a little bit
+  //   delay(150);
 
-   // wait for a second
-  delay(1000);
+  //   // Turn our current led back to black for the next loop around
+  //   leds[whiteLed] = CRGB::Black;
+  // }
+
+  // leds[XY(1, 1)] = CRGB::Red;
+  // FastLED.show();
+
+  // delay(500);
+
+  // leds[XY(1, 1)] = CRGB::Black;
+  // leds[XY(3, 1)] = CRGB::Red;
+  // FastLED.show();
+
+  byte x = 1;
+  byte y = 1;
+
+  leds[ XY( x, y) ] = CHSV( random8(), 255, 255);
+  FastLED.show();
 }
